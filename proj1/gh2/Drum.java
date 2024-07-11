eedfc8e89885c4460b6da379b8a01c558e99c034
Ronald_Luo <1239751758@qq.com>
2024-07-11 21:13:35 +0800

    proj 1: add harp and drum

---
+++ proj1/gh2/Drum.java
@@ -0,0 +1,52 @@
+package gh2;
+
+import deque.Deque;
+import deque.LinkedListDeque;
+
+public class Drum {
+    /** Constants. Do not change. In case you're curious, the keyword final
+     * means the values cannot be changed at runtime. We'll discuss this and
+     * other topics in lecture on Friday. */
+    private static final int SR = 44100;      // Sampling Rate
+    private static final double DECAY = 1.0; // energy decay factor
+
+    /* Buffer for storing sound data. */
+    private Deque<Double> buffer;
+
+    /* Create a guitar string of the given frequency.  */
+    public Drum(double frequency) {
+        buffer = new LinkedListDeque<>();
+        int capacity = (int) Math.round(SR / frequency);
+        for (int i = 0; i < capacity; i++) {
+            buffer.addLast(0.0);
+        }
+    }
+
+
+    /* Pluck the guitar string by replacing the buffer with white noise. */
+    public void pluck() {
+        for (int i = 0; i < buffer.size(); i++) {
+            double r = Math.random() - 0.5;
+            buffer.addLast(r);
+            buffer.removeFirst();
+        }
+    }
+
+    /* Advance the simulation one time step by performing one iteration of
+     * the Karplus-Strong algorithm.
+     */
+    public void tic() {
+        double sample1 = buffer.removeFirst();
+        double sample2 = buffer.get(0);
+        double newSample = (sample1 + sample2) / 2 * DECAY;
+        if (Math.random() < 0.5) {
+            newSample *= -1;
+        }
+        buffer.addLast(newSample);
+    }
+
+    /* Return the double at the front of the buffer. */
+    public double sample() {
+        return buffer.get(0);
+    }
+}
---
+++ proj1/gh2/DrumHero.java
@@ -0,0 +1,51 @@
+package gh2;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+/**
+ * A client that uses the synthesizer package to replicate a plucked guitar string sound
+ */
+public class DrumHero {
+    private static final int keyNum = 37;
+    public static final double[] CONCERTS = new double[keyNum];
+    public static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+
+    public static void main(String[] args) {
+        for (int i = 0; i < keyNum; i++) {
+            CONCERTS[i] = 440.0 * Math.pow(2, (i - 24.0) / 12.0);
+        }
+
+        /* create two guitar strings, for 37 concerts */
+        Drum[] strings = new Drum[keyNum];
+        for (int i = 0; i < keyNum; i++) {
+            strings[i] = new Drum(CONCERTS[i]);
+        }
+
+        while (true) {
+
+            /* check if the user has typed a key; if so, process it */
+            if (StdDraw.hasNextKeyTyped()) {
+                char key = StdDraw.nextKeyTyped();
+                int keyIndex = keyboard.indexOf(key);
+                if (keyIndex >= 0) {
+                    strings[keyIndex].pluck();
+                }
+            }
+
+            /* compute the superposition of samples */
+            double sample = 0;
+            for (int i = 0; i < keyNum; i++) {
+                sample += strings[i].sample();
+            }
+
+            /* play the sample on standard audio */
+            StdAudio.play(sample);
+
+            /* advance the simulation of each guitar string by one step */
+            for (int i = 0; i < keyNum; i++) {
+                strings[i].tic();
+            }
+        }
+    }
+}
+
