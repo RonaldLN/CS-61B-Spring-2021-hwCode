35db413cf280f5c7d4bd640ca4a519e9e1a7b84b
Ronald_Luo <1239751758@qq.com>
2024-07-20 02:26:41 +0800

    lab 4: get `lab1/Collatz.java`'s state back to the old correct state

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,28 +2,22 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
-
-    /** Buggy implementation of nextNumber! */
+    /** If n is even, return n / 2. If n is odd, return 3 * n + 1. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
         }
-        System.out.println();
     }
 }
-
 
