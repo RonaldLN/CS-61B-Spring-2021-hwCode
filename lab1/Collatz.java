dd0d96ed51bfc3303c4542334c2dce88b3d86f98
Ronald_Luo <1239751758@qq.com>
2024-07-19 22:29:21 +0800

    lab 1: finish Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -2,10 +2,22 @@
  *  @author YOUR NAME HERE
  */
 public class Collatz {
+    /** If n is even, return n / 2. If n is odd, return 3 * n + 1. */
+    public static int nextNumber(int n) {
+        if (n % 2 == 0) {
+            return n / 2;
+        } else {
+            return 3 * n + 1;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.print(n + " ");
+        }
     }
 }
 
