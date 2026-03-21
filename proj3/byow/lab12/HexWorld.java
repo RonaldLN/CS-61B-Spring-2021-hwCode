81490fb7bd7aacbb8cce84aa635c850890239043
Ronald_Luo <1239751758@qq.com>
2026-03-21 16:16:10 +0800

    lab 12: finish `HexWorld` and answer questions

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,91 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final long SEED = 2873123;
+    private static final Random RANDOM = new Random(SEED);
+
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(3);
+        switch (tileNum) {
+            case 0: return Tileset.WALL;
+            case 1: return Tileset.FLOWER;
+            case 2: return Tileset.GRASS;
+            default: return Tileset.NOTHING;
+        }
+    }
+
+    private static void addHexagon(TETile[][] world, int s, TETile type, int startX, int startY) {
+        int length = 3 * s - 2;
+
+        for (int dy = 0; dy < s; dy++) {
+            for (int dx = dy; dx < length - dy; dx++) {
+                world[startX + dx][startY + 1 + dy] = type;
+                world[startX + dx][startY - dy] = type;
+            }
+        }
+    }
+
+    private static void singleHexagonWorld(int s) {
+        final int PADDING = 1;
+        final int WIDTH = 3 * s - 2 + PADDING * 2;
+        final int HEIGHT = s * 2 + PADDING * 2;
+
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        addHexagon(world, s, Tileset.FLOWER, PADDING, PADDING + s - 1);
+
+        ter.renderFrame(world);
+    }
+
+    private static void tesselationOfHexagonsWorld(int s) {
+        final int PADDING = 2;
+        final int WIDTH = 3 * s - 2 + 4 * (2 * s - 1) + PADDING * 2;
+        final int HEIGHT = s * 10 + PADDING * 2;
+
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        int startX = PADDING;
+        int startY = s * 3 - 1 + PADDING;
+        int lineCount = 3;
+        for (int i = 0; i < 5; i++) {
+            int lineStartX = startX, lineStartY = startY;
+            for (int j = 0; j < lineCount; j++) {
+                addHexagon(world, s, randomTile(), lineStartX, lineStartY);
+                lineStartX += 2 * s - 1;
+                lineStartY -= s;
+            }
+
+            if (i < 2) {
+                startY += s * 2;
+                lineCount += 1;
+            } else {
+                startX += 2 * s - 1;
+                startY += s;
+                lineCount -= 1;
+            }
+        }
+
+        ter.renderFrame(world);
+    }
+
+    public static void main(String[] args) {
+        // singleHexagonWorld(5);
+        tesselationOfHexagonsWorld(3);
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -4,22 +4,22 @@
 After looking at your own implementation, consider the implementation provided near the end of the lab.
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
-Answer:
+Answer: The provided implementation uses helper classes like Position and more helper methods than my own, which I think creates better layers of abstraction and makes the code much cleaner and easier to understand.
 
 -----
 
 **Can you think of an analogy between the process of tessellating hexagons and randomly generating a world using rooms and hallways?
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
-Answer:
+Answer: Adding each hexagon into the tessellation is just like adding individual room or hallway piece into the entire world.
 
 -----
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
-Answer:
+Answer: I would start by writing methods to draw individual components, as well as corresponding helper methods for like calculating positions, to build a good abstraction layer.
 
 -----
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: A hallway has more than an exits, while a room only has one.
